62191dc95a797d2caffd1a43eb39747d5672aa1e
Daniel Schechter <schechterdani@gmail.com>
2018-08-04 11:34:03 +0300

    validations updated

--- VBeat/Models/ShowModel.cs
+++ VBeat/Models/ShowModel.cs
@@ -10,16 +10,28 @@ namespace VBeat.Models
     {
         [Key]
         public int ShowId { get; set; }
+
         [Required]
         [Display(Name = "Show Name")]
         [StringLength(20, MinimumLength = 3)]
         public string ShowName { get; set; }
         public virtual ICollection<ArtistModel> ArtistList { get; set; }
+
         [Required]
-        [StringLength(20, MinimumLength = 3)]
+        [StringLength(15, MinimumLength = 3)]
         public string Country { get; set; }
+
+        [Required]
+        [Display(Name = "Street Name")]
+        [StringLength(20, MinimumLength = 3)]
         public string StreetName { get; set; }
+
+        [Required]
+        [Display(Name = "House Name")]
         public int HouseNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Show Time")]
         public DateTime ShowTime { get; set; }
     }
 }
--- VBeat/Models/SongModel.cs
+++ VBeat/Models/SongModel.cs
@@ -10,12 +10,25 @@ namespace VBeat.Models
     {
         [Key]
         public int SongId { get; set; }
+
+        [Required]
+        [Display(Name = "Song Name")]
         public string SongName { get; set; }
+
         virtual public ICollection<ArtistModel> ArtistList { get; set; }
+
+        [Required]
         public string Genre { get; set; }
+
+        [Required]
         public string SongPath { get; set; }
+
         public string SongImagePath { get; set; }
+
+        [Required]
         public DateTime ReleaseDate { get; set; }
+
+        [Required]
         public DateTime AddedDate { get; set; }
     }
 }
