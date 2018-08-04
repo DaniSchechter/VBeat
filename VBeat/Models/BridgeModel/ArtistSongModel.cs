496a9e7533adfb767a415a5cd8a080fab624c9b1
QuestionGuy <guyishay2@gmail.com>
2018-08-04 15:59:28 +0300

    adding bridge model artist song model

---
+++ VBeat/Models/BridgeModel/ArtistSongModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VBeat.Models.BridgeModel
+{
+    public class ArtistSongModel
+    {
+        public int UserId { get; set; }
+        public ArtistModel Artist { get; set; }
+
+        public int SongId { get; set; }
+        public SongModel Song { get; set; }
+        
+    }
+}
