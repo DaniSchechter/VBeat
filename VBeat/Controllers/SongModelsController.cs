acaf67ce3bae3cd05f2ce7f94bad8bea27222250
QuestionGuy <guyishay2@gmail.com>
2018-08-05 20:55:28 +0300

    automatically setting added date on the add song page

--- VBeat/Controllers/SongModelsController.cs
+++ VBeat/Controllers/SongModelsController.cs
@@ -53,10 +53,11 @@ namespace VBeat.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("SongId,SongName,Genre,SongPath,SongImagePath,ReleaseDate,AddedDate")] SongModel songModel)
+        public async Task<IActionResult> Create([Bind("SongId,SongName,Genre,SongPath,SongImagePath,ReleaseDate")] SongModel songModel)
         {
             if (ModelState.IsValid)
             {
+                songModel.AddedDate = DateTime.UtcNow;
                 songModel.SongImagePath = "/images/" + songModel.SongImagePath;
                 _context.Add(songModel);
                 await _context.SaveChangesAsync();
