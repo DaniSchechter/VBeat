36f12c40f1ca27760a4e6211c13a4d6c3e907657
QuestionGuy <guyishay2@gmail.com>
2018-08-07 12:44:55 +0300

    adding session manager interface

---
+++ VBeat/Models/Session/SessionFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VBeat.Models.Session
+{
+    public class SessionFactory
+    {
+        public static SessionManager GetSessionManager()
+        {
+            return null;
+        }
+    }
+}
---
+++ VBeat/Models/Session/SessionManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VBeat.Models.Session
+{
+    interface SessionManager
+    {
+        UserModel GetUserModel();
+        ArtistModel GetArtistModel();
+    }
+}
