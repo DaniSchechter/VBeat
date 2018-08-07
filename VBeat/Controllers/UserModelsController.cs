ae3727137fbf0c8c70d40d615e391354b40a3c01
yuvalbir10 <yuvalbir10@gmail.com>
2018-08-07 16:18:25 +0300

    notsignedInLayout

--- VBeat/Controllers/UserModelsController.cs
+++ VBeat/Controllers/UserModelsController.cs
@@ -150,5 +150,12 @@ namespace VBeat.Controllers
         {
             return _context.Users.Any(e => e.UserId == id);
         }
+
+        
+       /* public async Task<IActionResult> SignIn()
+        {
+
+        }*/
+
     }
 }
