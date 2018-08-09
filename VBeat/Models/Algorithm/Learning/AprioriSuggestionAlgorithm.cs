5796b6578860ef02b61b71244c9242ffcf8015e8
QuestionGuy <guyishay2@gmail.com>
2018-08-09 12:48:25 +0300

    making suggestion algorithm a singleton to prevent excess training

---
+++ VBeat/Models/Algorithm/Learning/AprioriSuggestionAlgorithm.cs
@@ -0,0 +1,36 @@
+﻿using Accord.MachineLearning.Rules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VBeat.Models.Algorithm.Learning
+{
+    public class AprioriSuggestionAlgorithm : SuggestionAlgorithm
+    {
+        private static AprioriSuggestionAlgorithm instance = null;
+        public static AprioriSuggestionAlgorithm GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new AprioriSuggestionAlgorithm();
+            }
+            return instance;
+        }
+
+        private AprioriSuggestionAlgorithm()
+        {
+
+        }
+        public void Train()
+        {
+
+        }
+
+        public List<SongModel> Suggset(PlaylistModel playlist)
+        {
+        
+            
+        }
+    }
+}
