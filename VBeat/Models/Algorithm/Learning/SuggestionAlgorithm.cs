f65c4f338fa49bfe140a346b92400193fc09587d
QuestionGuy <guyishay2@gmail.com>
2018-08-09 12:38:29 +0300

    adding learning algorithm interface

---
+++ VBeat/Models/Algorithm/Learning/SuggestionAlgorithm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VBeat.Models.Algorithm.Learning
+{
+    interface SuggestionAlgorithm
+    {
+        List<SongModel> Suggset(PlaylistModel playlist);
+    }
+}
