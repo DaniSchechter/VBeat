b8f091785a670f273fea3d2a6ccf04e15828fe9c
Daniel Schechter <schechterda@cs.colman.ac.il>
2018-08-11 22:04:27 +0300

    finish view of playlist before adding music player

--- VBeat/wwwroot/js/site.js
+++ VBeat/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 $(document).ready(function () {
     $('.carousel').carousel();
 });
+
+$('div.playlist-song-container').hover(function () {
+    $(this).css("background-color", "black");
+    $(this).children('img').attr("src", "/images/play-button.png")
+
+});
+
+$('div.playlist-song-container').mouseleave(function () {
+    $(this).css("background-color", "transparent");
+    $(this).children('img').attr("src", "/images/speaker.png")
+});
+
+$('.add-song-to-audio-player').hover(function () {
+    $(this).css("cursor", "pointer");
+});
