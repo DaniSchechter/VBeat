e34fc885d6c5d4887db5ff5cef57cc9f9e8ab0e3
QuestionGuy <guyishay2@gmail.com>
2018-08-11 19:56:14 +0300

    writing a controller to test the apriori algorithm

---
+++ VBeat/Controllers/Test/AprioriController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VBeat.Models;
+using VBeat.Models.Algorithm.Learning;
+
+namespace VBeat.Controllers.Test
+{
+    public class AprioriController : Controller
+    {
+        AprioriSuggestionAlgorithm suggestionAlgorithm;
+        VBeatDbContext _context;
+
+        public AprioriController()
+        {
+            _context = new VBeatDbContext();
+            suggestionAlgorithm = AprioriSuggestionAlgorithm.GetInstance();
+        }
+
+
+        public List<int> Test(int playlistId)
+        {
+
+            suggestionAlgorithm.Train(_context.Playlists
+                .Where(p => p.PlaylistId != playlistId).ToList());
+            List<int> intList = suggestionAlgorithm.Suggset(_context.Playlists.Where(p => p.PlaylistId == playlistId).FirstOrDefault());
+            return intList;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
