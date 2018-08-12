df38d82b4baeeb6ba6358524c5c86b357db84fab
Alexander Schechter <schechteral@cs.colman.ac.il>
2018-08-12 19:37:49 +0300

    add plus btn to add the song to playlist

--- VBeat/wwwroot/js/site.js
+++ VBeat/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 $(document).ready(function () {
     $('.carousel').carousel();
 });
+
+$('.table-cell').hover(function () {
+    $(this).find("img").show();
+});
+
+$('.table-cell').mouseleave(function () {
+    $(this).children("a").children("img").hide();
+});
+
+/*TO do --------------------------------------------------------------------------*/
+$(".add-song-to-playlist").click(function() {
+    alert("to do - add song to playlist");
+});
