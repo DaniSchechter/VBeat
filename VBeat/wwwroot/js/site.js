593e050b48eadef2bf2f4e824b0e43bed40ac551
Daniel Schechter <schechterda@cs.colman.ac.il>
2018-08-12 13:29:43 +0300

    add audio player

--- VBeat/wwwroot/js/site.js
+++ VBeat/wwwroot/js/site.js
@@ -17,3 +17,15 @@ $('div.playlist-song-container').mouseleave(function () {
 $('.add-song-to-audio-player').hover(function () {
     $(this).css("cursor", "pointer");
 });
+$('.add-song-to-audio-player').click(function () {
+    var path = $(this).siblings('.song-path-container').children('p');
+    var audio = document.getElementById("myAudio"); 
+    var answer = audio.canPlayType('mp4; codecs = "mp4a.40.5"');
+    alert(answer);
+});
+
+
+//_________________________________________________________ Just to not forget something
+$('.add-playlist-to-library').click(function () {
+    alert("To DO :add playlist to list of playlists")
+});
