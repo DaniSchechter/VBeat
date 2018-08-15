7c10bc17833530b63487ec57e9feb2dd1122afde
QuestionGuy <guyishay2@gmail.com>
2018-08-15 15:01:58 +0300

    fixing it so you can't access pages without auth

---
+++ VBeat/Models/ActionFilter/GlobalAuthFilter.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Session;
+using VBeat.Models.Consts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace VBeat.Models.ActionFilter
+{
+    public class GlobalAuthFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            
+            if (context.ActionDescriptor.DisplayName == "VBeat.Controllers.UserModelsController.SignIn (VBeat)" || context.ActionDescriptor.DisplayName == "VBeat.Controllers.UserModelsController.Create (VBeat)" || context.ActionDescriptor.DisplayName == "VBeat.Controllers.ArtistModelsController.Create (VBeat)")
+            {
+                return;
+            }
+
+            if (!context.HttpContext.Session.GetInt32(SessionConsts.UserId).HasValue)
+            {
+                context.Result = new RedirectToRouteResult(
+                       new RouteValueDictionary(new { controller = "UserModels", action = "SignIn" })
+                );
+
+                
+
+                context.Result.ExecuteResultAsync(((Controller)context.Controller).ControllerContext);
+            }
+
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            
+        }
+    }
+}
--- VBeat/Startup.cs
+++ VBeat/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Serialization;
 using VBeat.Models;
+using VBeat.Models.ActionFilter;
 
 namespace VBeat
 {
@@ -24,14 +25,21 @@ namespace VBeat
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDistributedMemoryCache();
+            services.AddScoped<GlobalAuthFilter>();
+
             services.AddSession(options =>
             {
                 options.IdleTimeout = TimeSpan.FromDays(30);
                 options.Cookie.HttpOnly = true;
             });
             services.AddDbContext<VBeatDbContext>();
-            services.AddMvc();
-                //.AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+            services.AddMvc().AddMvcOptions(
+                options =>
+                {
+                    options.Filters.AddService(typeof(GlobalAuthFilter));
+                }
+            );
+            //.AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,7 +65,7 @@ namespace VBeat
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=UserModels}/{action=SignIn}/{id?}");
-            }); 
+            });
         }
     }
 }
