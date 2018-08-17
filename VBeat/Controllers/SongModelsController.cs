a11d68bbe5bad850878acd8f24036163da1f9d27
QuestionGuy <guyishay2@gmail.com>
2018-08-17 11:47:16 +0300

    writing file upload helper

--- VBeat/Controllers/SongModelsController.cs
+++ VBeat/Controllers/SongModelsController.cs
@@ -115,7 +115,7 @@ namespace VBeat.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("SongId,SongName,Genre,SongPath,SongImagePath,ReleaseDate")] SongModel songModel)
+        public async Task<IActionResult> Create([Bind("SongId,SongName,Genre,SongPath,ReleaseDate")] SongModel songModel, IFormFile SongImagePath)
         {
             if (ModelState.IsValid)
             {
---
+++ VBeat/Models/FileHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VBeat.Models
+{
+    public class FileHelper
+    {
+        public static string SaveFile(IFormFile formFile, string dir, string filename)
+        {
+            dir = dir.Trim('.');
+            using (Stream stream = formFile.OpenReadStream())
+            {
+                filename = "/" + dir + "/" + System.Guid.NewGuid() + "-" + filename;
+                using (StreamWriter writer = new StreamWriter(filename))
+                {
+                    byte[] buffer = new byte[2048];
+                    int bRead = 0;
+                    while ((bRead = stream.Read(buffer, 0, buffer.Length)) != -1)
+                    {
+                        writer.BaseStream.Write(buffer, 0, bRead);
+                    }
+                }
+            }
+
+            return filename;
+        }
+    }
+}
