02903af52202648d20635f56401515b549f6f92d
QuestionGuy <guyishay2@gmail.com>
2018-08-17 11:56:59 +0300

    fixing directory building

--- VBeat/Models/FileHelper.cs
+++ VBeat/Models/FileHelper.cs
@@ -11,15 +11,15 @@ namespace VBeat.Models
     {
         public static string SaveFile(IFormFile formFile, string dir, string filename)
         {
-            dir = dir.Trim('.');
+            dir = dir.Trim('.', '~', '/');
             using (Stream stream = formFile.OpenReadStream())
             {
-                filename = "/" + dir + "/" + System.Guid.NewGuid() + "-" + filename;
+                filename = "~/" + dir + "/" + System.Guid.NewGuid() + "-" + filename;
                 using (StreamWriter writer = new StreamWriter(filename))
                 {
                     byte[] buffer = new byte[2048];
                     int bRead = 0;
-                    while ((bRead = stream.Read(buffer, 0, buffer.Length)) != -1)
+                    while ((bRead = stream.Read(buffer, 0, buffer.Length)) != 0)
                     {
                         writer.BaseStream.Write(buffer, 0, bRead);
                     }
